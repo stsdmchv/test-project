c0569d99e12186d0e3f7e551944c0a7409d617bb
Stanislav Demichev <stsdmchv@icloud.com>
2022-10-17 19:13:15 +0300

    Change div for cards on CardGroup

--- src/components/userCards.js
+++ src/components/userCards.js
@@ -1,19 +1,23 @@
-import {Card} from "react-bootstrap";
+import {Button, Card, CardGroup} from "react-bootstrap";
 import './userCards.css'
-
 const users = []
-users.push({ id: '1', firstName: 'Белгатой', lastName:'Ширехзон' })
-users.push({ id: '2', firstName: 'Шали', lastName:'Арми' })
-users.push({ id: '3', firstName: 'Урус-Мартан', lastName:'Кур-котан' })
-
+users.push({id: '1', firstName: 'Белгатой', lastName: 'Ширехзон'})
+users.push({id: '2', firstName: 'Шали', lastName: 'Арми'})
+users.push({id: '3', firstName: 'Урус-Мартан', lastName: 'Кур-котан'})
 const UserCards = () => (
   users.map((user, index) =>
-    <div className="Body">
-      <div className="UserCard">
-        <Card>{user.id} <br /> {user.firstName} {user.lastName}</Card>
-      </div>
-    </div>
+    <CardGroup >
+        <Card >
+          <Card.Header>{user.id}</Card.Header>
+          <Card.Body>
+            <Card.Title>{user.firstName} {user.lastName}</Card.Title>
+          </Card.Body>
+          <Card.Footer>
+            <Button variant="primary">Delete</Button>
+            <Button>Edit</Button>
+          </Card.Footer>
+        </Card>
+    </CardGroup>
   )
 )
-
 export default UserCards;
