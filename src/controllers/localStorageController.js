56ec49a08f0f06b32971278d6d2277059ac56343
Stanislav Demichev <stsdmchv@icloud.com>
2022-10-20 02:46:16 +0300

    Add variants for working with localStorage.

--- src/controllers/localStorageController.js
+++ src/controllers/localStorageController.js
@@ -1,15 +1,19 @@
 import React from "react";
 
 //@desc   Get values from localStorage
-export const getValuesLS = () => {
-  const values = [],
-    keys = Object.keys(localStorage)
-  let iterator = keys.length
+export const getValuesLS = (key = 0) => {
+  if (key === 0){
+    const values = [],
+      keys = Object.keys(localStorage)
+    let iterator = keys.length
 
-  while (iterator--){
-    values.push(localStorage.getItem(keys[iterator]))
+    while (iterator--){
+      values.push(localStorage.getItem(keys[iterator]))
+    }
+    return values
   }
-  return values
+
+  return localStorage.getItem(key)
 }
 
 //@desc   Put values from localStorage
